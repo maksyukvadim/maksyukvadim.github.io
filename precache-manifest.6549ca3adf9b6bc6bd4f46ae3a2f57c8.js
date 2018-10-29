0e5f1accb51b9120bc2d03d6a4c1232a95db2ffa
Vadym Maksyuk <maksyukvadim@gmail.com>
2018-10-29 15:23:37 +0200

    new site

---
+++ precache-manifest.6549ca3adf9b6bc6bd4f46ae3a2f57c8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c4f3191a49b8cebddc17",
+    "url": "/static/js/runtime~main.c4f3191a.js"
+  },
+  {
+    "revision": "9c06c1f38585e80f5de5",
+    "url": "/static/js/main.9c06c1f3.chunk.js"
+  },
+  {
+    "revision": "6e99cd879952aafe69c9",
+    "url": "/static/js/4.6e99cd87.chunk.js"
+  },
+  {
+    "revision": "7d04b7cc9c69b7f5a11b",
+    "url": "/static/js/3.7d04b7cc.chunk.js"
+  },
+  {
+    "revision": "3d13380e16cc2db8c32a",
+    "url": "/static/js/2.3d13380e.chunk.js"
+  },
+  {
+    "revision": "b3caf9e62711fc9a4c32",
+    "url": "/static/js/0.b3caf9e6.chunk.js"
+  },
+  {
+    "revision": "9c06c1f38585e80f5de5",
+    "url": "/static/css/main.071ea8ae.chunk.css"
+  },
+  {
+    "revision": "c254c130356fbd8e2d4a3b6004a3b228",
+    "url": "/index.html"
+  }
+];
