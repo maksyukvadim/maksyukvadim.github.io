9364f71d7b0eb4e0b2d5c21728cf797ce3928bee
Вадим Максюк <maksyukvadim@gmail.com>
2020-07-21 09:42:16 +0300

    new site

---
+++ precache-manifest.d08c8b122f693ebf1b7ae2298220679d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4e9a5d1786c7ecc1f2e93a87ad00a7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9423dd2524c7e1251a85",
+    "url": "/static/js/2.d7199a65.chunk.js"
+  },
+  {
+    "revision": "575ed734712d6c7c9a32d45f86679d73",
+    "url": "/static/js/2.d7199a65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb8dc77460531c8e6f91",
+    "url": "/static/js/main.c4f2aa71.chunk.js"
+  },
+  {
+    "revision": "1805414ce4062d7d010e",
+    "url": "/static/js/runtime-main.dd4b23b4.js"
+  },
+  {
+    "revision": "24525eca6e11f1508f42d5e46966b83b",
+    "url": "/static/media/404.24525eca.png"
+  },
+  {
+    "revision": "ebbc4701d45c596cabfd8c62b6fc3233",
+    "url": "/static/media/GT-America-Extended-Bold.ebbc4701.otf"
+  },
+  {
+    "revision": "78b3d2f622bf67b536bf64e2ecd80164",
+    "url": "/static/media/GT-America-Extended-Medium.78b3d2f6.otf"
+  }
+]);
